670cff5bf52011af6e09e6c24558a7e9590c2d50
rodolfocoding <rodolfo.coding@gmail.com>
2024-09-24 12:20:05 -0300

    chore: init project

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -12,11 +12,13 @@ class PostController {
         });
       }
 
-      const prompt: string = `Crie uma postagem para o LinkedIn sobre um tópico relevante e atual na área de ${subject}.
-      A postagem deve destacar uma tendência, inovação ou boas práticas, 
-      com foco em como isso impacta profissionais da área. Ela deve ser informativa, 
-      inspiradora e promover o engajamento da comunidade, convidando o público a refletir ou compartilhar suas opiniões.
-      Inclua uma chamada para ação no final para incentivar a interação.`;
+      const prompt: string = `Crie uma postagem altamente envolvente para o LinkedIn sobre um tópico emergente e relevante na área de ${subject}. 
+      A postagem deve explorar profundamente uma tendência inovadora, 
+      tecnologia disruptiva ou boas práticas que estejam moldando o futuro do setor, 
+      destacando exemplos reais de como isso já está impactando profissionais, 
+      empresas e o mercado. Certifique-se de trazer insights valiosos que possam ajudar os leitores a refletirem sobre os desafios e oportunidades que essa inovação traz. Use um tom inspirador e informativo, convidando a comunidade a participar da discussão com seus próprios pontos de vista e experiências.
+      Além disso, promova o engajamento ativo, criando uma chamada para ação estratégica que incentive os leitores a compartilhar suas opiniões, soluções práticas ou perguntas, criando um espaço para debates construtivos. Ao final, inclua uma provocação instigante,
+      como: "Como você está se preparando para essa transformação no ${subject}? Vamos trocar ideias nos comentários!`;
 
       const text = await GeminiAIService.sendPrompt(prompt);
 
